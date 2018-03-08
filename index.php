39b1f0d12897dec350605909a17e8a6fb57effb9
gilles Delmotte <gilles.delmotte@outlook.be>
2018-03-08 12:23:17 +0100

    delete jQuery

--- index.php
+++ index.php
@@ -20,34 +20,9 @@
 <div>
     <p>Voici les lettres que tu as déjà essayées&nbsp;: <span id="tried-letters"></span></p>
 </div>
-<div id="playing">
-    <form action="index.php"
-          method="post">
-        <fieldset>
-            <legend>Il te reste <span id="remaining-trials">0</span> essais pour sauver ta peau
-            </legend>
-            <div>
-                <label for="select-letter">Choisis ta lettre</label>
-                <select name="select-letter"
-                        id="select-letter">
-                </select>
-                <input type="submit"
-                       value="essayer cette lettre">
-            </div>
-        </fieldset>
-    </form>
+<div id="game">
+    
 </div>
-<div id="winning">
-    <p class="bg-success lead">Bravo&nbsp;! Tu as trouvé le mot
-        «&nbsp;<b class="word"></b>&nbsp;». <a href="index.php">Recommence&nbsp;!</a>
-    </p>
-</div>
-<div id="losing">
-    <p class="bg-danger lead">OOOps&nbsp;! Tu sembles bien mort&nbsp;! Le mot à trouver était
-        «&nbsp;<b class="word"></b>&nbsp;». <a href="index.php">Recommence&nbsp;!</a>
-    </p>
-</div>
-<script src="scripts/jquery.min.js"></script>
 <script src="scripts/main.js"></script>
 </body>
 </html>
--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,88 @@
-$('#winning').hide();
-$('#losing').hide();
+const ajax = (url = '', fn, method = 'GET', data = null) => {
+	if(!url || !fn) return
+	let req = new XMLHttpRequest();
+	req.onreadystatechange = fn;
+	req.open(method, url, true);
+	req.send(data);
+}
 
-var gameData = {};
+
+const play = () =>{
+	document.querySelector('#game').insertAdjacentHTML("beforeend", gameData[`${gameData.state}Html`])
+	document.querySelector('form').addEventListener('submit', e => {
+		e.preventDefault();
+		const selectLetter = document.querySelector('#select-letter')
+		const triedLetter = selectLetter.options[selectLetter.selectedIndex].value
+		console.log(triedLetter)
+		gameData.alphabet = gameData.alphabet.replace(triedLetter,'');
+	 	gameData.triedLetters += triedLetter;
+
+	 	let replacementString = '';
+		let letterFound = false;
+		for(let i = 0; i<= gameData.lettersCount; i++){
+			if(gameData.word.charAt(i) === triedLetter.toUpperCase()){
+				replacementString += triedLetter
+				letterFound = true;
+			}else{
+			replacementString += gameData.replacementString.charAt(i);
+		}
+		};
+
+		gameData.replacementString = replacementString;
+
+		if(!letterFound){
+			gameData.trials +=1;
+			gameData.remainingTrials = gameData.maxTrials - gameData.trials;
+			if(gameData.remainingTrials === 0){
+			//afficher le msg de defaite
+			gameData.state = gameData.states[2];
+			}
+		}else{
+		console.log(gameData);
+		if(gameData.replacementString.toUpperCase() === gameData.word){
+			alert('tu as gagné !')
+			//afficheer le msg de succes 
+			gameData.state = gameData.states[1];
+		}
+	}
+
+	render();
+
+	})
+	render();
+}
+
+const render = () => {
+
+	document.querySelector('#max-trials').textContent = gameData.maxTrials;
+	document.querySelector('#letters-count').textContent = gameData.lettersCount;
+	document.querySelector('#replacement-string').textContent = gameData.replacementString;
+	document.querySelector('#tried-letters').textContent = gameData.triedLetters;
+
+	document.querySelectorAll('#select-letter option').forEach(opt => opt.remove())
+	gameData.alphabet.split('').forEach(letter =>
+		document.querySelector('#select-letter').insertAdjacentHTML("beforeend", `<option value="${letter}">${letter}</option>`)
+	)		
+	
+
+	document.querySelector('#image-file').src = `images/pendu${gameData.trials}.gif`;
+	if(gameData.state !== gameData.states[0]){
+		document.querySelector(`#${gameData.states[0]}`).remove();
+		document.querySelector('#game').insertAdjacentHTML("beforeend", gameData[`${gameData.state}Html`])
+		let gameStateNode = document.querySelector(`#${gameData.state}`);
+		gameStateNode.querySelector('.word').textContent = gameData.word;
+
+
+	}else{
+
+		document.querySelector('#remaining-trials').textContent = gameData.remainingTrials;
+
+
+	}	
+}
+
+
+const gameData = {};
 gameData.maxTrials = 8;
 gameData.trials = 0;
 gameData.remainingTrials = gameData.maxTrials;
@@ -13,85 +94,28 @@ gameData.triedLetters = ''
 gameData.states = ['playing', 'winning', 'losing']
 gameData.state = gameData.states[0];
 
+gameData.states.forEach(state => {gameData[`${state}Html`] = ''})
 
-$.get('/api/index.php', function(word){
-	gameData.word = word;
-	gameData.lettersCount = word.length;
-	gameData.replacementString = word.replace(/./g,'*');
-	render();
-	//select();
+
+
+ajax('/api/index.php', function(e){
+	if(e.target.readyState !== 4 || e.target.status !== 200) return
+	gameData.word = e.target.responseText;
+	gameData.lettersCount = gameData.word.length;
+ 	gameData.replacementString = gameData.word.replace(/./g,'*');
+
+ 	gameData.states.forEach(state => {
+ 		ajax(`../partials/${state}.html`, e => {
+ 			if(e.target.readyState !== 4 || e.target.status !== 200) return
+ 			gameData[`${state}Html`] = e.target.responseText
+ 			if(state === gameData.states[0]) play()
+ 		})
+ 		
+ 	})
+
 });
 
 
-$('form').on('submit', function(e){
-	e.preventDefault();
-	/* code à exécuter quand on soumet une lettre */
-	let triedLetter = $('#select-letter option:selected').val();
-	// retirer de la liste => mettre a jour l'alphabet
-	gameData.alphabet = gameData.alphabet.replace(triedLetter,'');
-	gameData.triedLetters += triedLetter;
-	// verifier si la lettre est dans le mot 
-	let replacementString = '';
-	let letterFound = false;
-	for(let i = 0; i<= gameData.lettersCount; i++){
-		if(gameData.word.charAt(i) === triedLetter.toUpperCase()){
-			replacementString += triedLetter
-			letterFound = true;
-		}else{
-			replacementString += gameData.replacementString.charAt(i);
-		}
-	};
-	gameData.replacementString = replacementString;
-
-	if(!letterFound){
-		gameData.trials +=1;
-		gameData.remainingTrials = gameData.maxTrials - gameData.trials;
-		if(gameData.remainingTrials === 0){
-			//afficher le msg de defaite
-			gameData.state = gameData.states[2];
-		}
-	}else{
-		console.log(gameData);
-		if(gameData.replacementString.toUpperCase() === gameData.word){
-			alert('tu as gagné !')
-			//afficheer le msg de succes 
-			gameData.state = gameData.states[1];
-		}
-	}
-
-	render();
-});
-
-
-function render(){
-	$('#max-trials').text(gameData.maxTrials);
-	$('#letters-count').text(gameData.lettersCount);
-	$('#replacement-string').text(gameData.replacementString);
-	$('#select-letter').find('option').remove();
-	$('#tried-letters').text(gameData.triedLetters);	
-	// créer les option (utiliser split et foreach)
-	gameData.alphabet.split("").forEach(letter => 
-		$('#select-letter').append(`<option value="${letter}">${letter}</option>`)
-	);	
-	
-	gameData.states.forEach(function(state){
-		if(gameData.state === state){
-			$(`#${state}`).show();
-		}else{
-			$(`#${state}`).hide();
-		}
-	})
-
-	$('#remaining-trials').text(gameData.remainingTrials);
-	$('#image-file').attr('src',`images/pendu${gameData.trials}.gif`);
-	$('word').text(gameData.word);
-}
-	
-
-// on a 3 etats possible [winning, losing , playing]
-// mais il n'y a qu'un etat actif a la fois - playing
-// -> cacher les etats inactif .hide()
-// -> afficher l'etat actif .show()
 
 
 
